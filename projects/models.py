fbed5c7c991ff40fccae3332dc2fd3c4a36e1e29
Abu Zafer Md Fahim <113917600+AbuZaferMdFahim@users.noreply.github.com>
2023-05-08 00:22:17 +0600

    Update models.py

--- projects/models.py
+++ projects/models.py
@@ -21,7 +21,23 @@ class Project(models.Model):
         return self.title
     
     class Meta:
-        ordering = ['-created']
+        ordering = ['-vote_Ratio', '-vote_Total','title']
+    
+    @property
+    def reviewers(self):
+        queryset = self.review_set.all().values_list('owner__id',flat=True)
+        return queryset
+
+    @property
+    def getVoteCount(self):
+        reviews = self.review_set.all()
+        upVotes = reviews.filter(value = 'up').count()
+        totalVotes = reviews.count()
+
+        ratio = (upVotes /totalVotes) * 100 
+        self.vote_Total = totalVotes
+        self.vote_Ratio = ratio
+        self.save()
 
 class Review(models.Model):
     VOTE_TYPE = (
@@ -48,5 +64,3 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-
-
