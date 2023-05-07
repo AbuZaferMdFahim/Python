881fe45b1c709430a3f0de56718cd7db346863eb
Abu Zafer Md Fahim <113917600+AbuZaferMdFahim@users.noreply.github.com>
2023-05-08 00:20:23 +0600

    Update forms.py

--- projects/forms.py
+++ projects/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 from django import forms
-from .models import Project
+from .models import Project,Review
 
 class ProjectForm(ModelForm):
     class Meta():
@@ -11,8 +11,23 @@ class ProjectForm(ModelForm):
             'tags': forms.CheckboxSelectMultiple(),
         }
 
-        def __init__(self, *args, **Kwargs):
-            super(ProjectForm, self).__init__(*args, **Kwargs)
+    def __init__(self, *args, **Kwargs):
+        super(ProjectForm, self).__init__(*args, **Kwargs)
 
-            for name, field in self.fields.items():
-                field.widget.attrs.update({'class': 'input'})
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['value','body']
+        labels = {
+            'value': 'Place your Vote',
+            'body' : 'Add a comment woth your vote'
+        }
+    def __init__(self, *args, **Kwargs):
+        super(ReviewForm, self).__init__(*args, **Kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
